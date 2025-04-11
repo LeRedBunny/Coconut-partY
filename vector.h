9f45b9e4dc9d6e4649e82e91a5c3f3682feaafe9
LeRedBunny <alexi.toutou22@gmail.com>
2025-04-11 13:13:28 +0200

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,20 @@
+typedef struct {
+	int x, y;
+} Vector;
+
+Vector vector (int x, int y) {
+	Vector v;
+	v.x = x;
+	v.y = y;
+	return v;
+}
+
+Vector add (Vector u, Vector v) {
+	return vector(u.x + v.x, u.y + v.y);
+}
+
+Vector scale (Vector v, int scalar) {
+	return vector(scalar * v.x, scalar * v.y);
+}
+
+Vector DIRECTIONS[4] = {vector(1, 0), vector(0, 1), vector(-1, 0), vector(0, -1)}
